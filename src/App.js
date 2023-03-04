55d0298f2677d3b1891a852e186acaf0d972f469
chessrajat <raat66rat@gmail.com>
2023-03-04 10:57:37 +0530

    added: react-router and custom font

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import RootLayout from "./Layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+
+    </Route>
+  )
+)
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-blue-600 font-bold underline">Hello world!</h1>
-      <FontAwesomeIcon icon={faHome} />
-    </div>
+    <RouterProvider router={router} />
   );
 };
 
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="font-poppins">This is our home page</div>
+  )
+}
+
+export default Home
