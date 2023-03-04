5b28c8e1a8ce720c3435644369a025a241aaeb72
chessrajat <raat66rat@gmail.com>
2023-03-04 10:26:42 +0530

    tailwind css and fontawesome icons added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="text-3xl text-blue-600 font-bold underline">Hello world!</h1>
+      <FontAwesomeIcon icon={faHome} />
     </div>
   );
-}
+};
 
 export default App;
